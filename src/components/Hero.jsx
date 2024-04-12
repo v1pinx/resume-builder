8da278c4ebb7490a794600724683753a32d15d13
v1pinx <vkprajapati665@gmail.com>
2024-04-12 13:14:36 +0530

    update

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+const Hero = () => {
+    return (
+        <>
+            <h2>
+                Welcome to the hero section
+            </h2>
+        </>
+    )
+}
+
+export default Hero;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+
+const Navbar = () => {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/about">About</a>
+                </li>
+                <li>
+                    <a href="/services">Services</a>
+                </li>
+                <li>
+                    <a href="/contact">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
